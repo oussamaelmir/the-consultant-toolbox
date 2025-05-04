6f3b6e97b0f7c8d18acef9d3368dd186908b9db6
Oussama <oussama@Oussamas-MacBook-Pro.local>
2025-05-04 19:36:35 +0100

    HTML adaptation to the file paths

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve everything directly from the root of the deployed folder
-app.use(express.static(__dirname));
-
+app.use(express.static(__dirname, {
+    index: ['index.html'],      // still serve index.html for directories
+    extensions: ['html'],       // try appending .html for extensionless requests
+  }));
 // If you ever want a catch-all (e.g. for SPA), you can uncomment this:
 // app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
